42b1c9cc77c5475fe5abb01e6c7705b91cedc49a
root <you@example.com>
2023-02-14 15:26:32 -0800

    correction task 4 and task 5

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - needed
- * Return value 0
- */
+* main - needed
+* Return value 0
+*/
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
