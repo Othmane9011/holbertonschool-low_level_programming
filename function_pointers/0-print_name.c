bfa19c2871e5ead0ed7bac87429aaaffe121c960
root <you@example.com>
2023-03-22 22:39:28 -0700

    task 0 betty

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,11 +1,11 @@
 #include "function_pointers.h"
 
 /**
- * * print_name - print a name using the function supplied by the caller
- * * @name: the name to print
- * * @f: the function to call to print the name
- * *
- * */
+ * print_name - print a name using the function supplied by the caller
+ * @name: the name to print
+ * @f: the function to call to print the name
+ *
+ */
 void print_name(char *name, void (*f)(char *))
 {
 	if (f == NULL)
