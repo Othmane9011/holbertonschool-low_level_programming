ff59080b36011d3e338c059360454b510277d6dc
root <you@example.com>
2023-04-07 13:22:55 -0700

    betty

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -5,11 +5,11 @@
  ** add_node - function with two arguments
  * @head: double pointer to linked list
  * @str: string pointer
- * add_node: adding node
+ *
  * Description: adds a new node at the beginning
  * Return: address of new element
  */
-list_t *add_node(list_t **head, const char *str)
+list_t add_node(list_t **head, const char *str)
 {
 	int count = 0;
 		list_t *temp;
